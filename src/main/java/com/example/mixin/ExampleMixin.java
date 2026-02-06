5245b0a68bb8dd2738e072769ae112f483b7b8f9
aoksoryanchik <verych222@gmail.com>
2026-02-06 19:29:27 +0100

    Update ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -14,12 +14,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HeldItemRenderer.class)
 public class ExampleMixin {
-
     @Inject(method = "renderFirstPersonItem", at = @At("HEAD"))
     private void onRenderItem(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider consumers, int light, CallbackInfo ci) {
-        // Если функция ViewModel включена в меню мода
         if (ExampleMod.viewModelActive) {
-            // Смещаем руку по координатам из нашего меню (handX, handY, handZ)
             matrices.translate(ExampleMod.handX, ExampleMod.handY, ExampleMod.handZ);
         }
     }
