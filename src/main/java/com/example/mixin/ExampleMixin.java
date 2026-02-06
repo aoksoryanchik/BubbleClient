60ec41813dd45f2c24c36cc0ea5cd5389bf4bb69
aoksoryanchik <verych222@gmail.com>
2026-02-06 20:10:55 +0100

    Update ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,12 +1,10 @@
 package com.example.mixin;
 
 import com.example.ExampleMod;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.HeldItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.util.Hand;
-import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,10 +12,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HeldItemRenderer.class)
 public class ExampleMixin {
-    @Inject(method = "renderFirstPersonItem", at = @At("HEAD"))
-    private void onRenderItem(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider consumers, int light, CallbackInfo ci) {
+    @Inject(method = "renderItem", at = @At("HEAD"))
+    private void onRenderItem(float tickDelta, MatrixStack matrices, VertexConsumerProvider.Immediate vertexConsumers, net.minecraft.client.network.ClientPlayerEntity player, int light, CallbackInfo ci) {
         if (ExampleMod.viewModelActive) {
-            // Применяем смещение из настроек меню
             matrices.translate(ExampleMod.handX, ExampleMod.handY, ExampleMod.handZ);
         }
     }
