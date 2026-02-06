e1b8e2054f46549dae410c93b666f37f1699e9c4
aoksoryanchik <verych222@gmail.com>
2026-02-06 20:45:42 +0100

    Update ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,20 +1,9 @@
 package com.example.mixin;
 
-import com.example.ExampleMod;
 import net.minecraft.client.render.item.HeldItemRenderer;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.client.render.VertexConsumerProvider;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HeldItemRenderer.class)
 public class ExampleMixin {
-    @Inject(method = "renderItem", at = @At("HEAD"))
-    private void onRenderItem(float tickDelta, MatrixStack matrices, VertexConsumerProvider.Immediate vertexConsumers, net.minecraft.client.network.ClientPlayerEntity player, int light, CallbackInfo ci) {
-        if (ExampleMod.viewModelActive) {
-            matrices.translate(ExampleMod.handX, ExampleMod.handY, ExampleMod.handZ);
-        }
-    }
+    // Код удален по просьбе пользователя, чтобы не вызывать ошибок компиляции
 }
