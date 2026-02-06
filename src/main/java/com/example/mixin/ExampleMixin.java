1d7938ac60b998d4196f125b828462ebc7351054
aoksoryanchik <verych222@gmail.com>
2026-02-06 19:22:21 +0100

    Update ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,26 @@
 package com.example.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import com.example.ExampleMod;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.item.HeldItemRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Hand;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
+@Mixin(HeldItemRenderer.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
-	}
+    
+    @Inject(method = "renderFirstPersonItem", at = @At("HEAD"))
+    private void onRenderItem(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider consumers, int light, CallbackInfo ci) {
+        // Проверяем, включена ли функция ViewModel в главном файле
+        if (ExampleMod.viewModelActive) {
+            // Двигаем руку согласно переменным handX, handY, handZ из нашего меню
+            matrices.translate(ExampleMod.handX, ExampleMod.handY, ExampleMod.handZ);
+        }
+    }
 }
