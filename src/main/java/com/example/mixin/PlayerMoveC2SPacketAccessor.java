892ace7a5eff774c3f93a7cc382c379bffc9839a
aoksoryanchik <verych222@gmail.com>
2026-02-16 16:15:51 +0100

    Create PlayerMoveC2SPacketAccessor.java

---
+++ src/main/java/com/example/mixin/PlayerMoveC2SPacketAccessor.java
@@ -0,0 +1,15 @@
+package com.example.mixin;
+
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(PlayerMoveC2SPacket.class)
+public interface PlayerMoveC2SPacketAccessor {
+    @Accessor("yaw")
+    void setYaw(float yaw);
+
+    @Accessor("pitch")
+    void setPitch(float pitch);
+}
+
