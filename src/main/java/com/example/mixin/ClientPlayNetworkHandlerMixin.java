706731c4bbdee4d00893d9e6f15fa2041eab6221
aoksoryanchik <verych222@gmail.com>
2026-02-16 16:32:22 +0100

    Update ClientPlayNetworkHandlerMixin.java

--- src/main/java/com/example/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/com/example/mixin/ClientPlayNetworkHandlerMixin.java
@@ -12,7 +12,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
 
-    @Inject(method = "sendPacket(Lnet/minecraft/network/packet/Packet;)V", at = @At("HEAD"))
+    // Используем универсальное имя метода для 1.21
+    @Inject(method = "sendPacket", at = @At("HEAD"), cancellable = true)
     private void onSendPacket(Packet<?> packet, CallbackInfo ci) {
         if (ExampleMod.killaura && ExampleMod.targetFound && packet instanceof PlayerMoveC2SPacket movePacket) {
             PlayerMoveC2SPacketAccessor accessor = (PlayerMoveC2SPacketAccessor) movePacket;
@@ -21,3 +22,4 @@ public class ClientPlayNetworkHandlerMixin {
         }
     }
 }
+
