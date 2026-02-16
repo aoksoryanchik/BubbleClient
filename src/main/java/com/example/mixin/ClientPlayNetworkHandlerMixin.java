e1f0b7b8386fea53eb0a96a30aa729a0c812f788
aoksoryanchik <verych222@gmail.com>
2026-02-16 19:05:56 +0100

    Update ClientPlayNetworkHandlerMixin.java

--- src/main/java/com/example/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/com/example/mixin/ClientPlayNetworkHandlerMixin.java
@@ -11,9 +11,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
-
-    @Inject(method = "sendPacket", at = @At("HEAD"), cancellable = true, require = 0)
-    private void onSend(Packet<?> packet, CallbackInfo ci) {
+    @Inject(method = "sendPacket", at = @At("HEAD"), cancellable = true)
+    private void onSendPacket(Packet<?> packet, CallbackInfo ci) {
         if (ExampleMod.killaura && ExampleMod.targetFound && packet instanceof PlayerMoveC2SPacket) {
             PlayerMoveC2SPacketAccessor accessor = (PlayerMoveC2SPacketAccessor) packet;
             accessor.setYaw(ExampleMod.serverYaw);
